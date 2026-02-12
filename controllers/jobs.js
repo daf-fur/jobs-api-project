64cbe915976fe7c9961ed5d7df0a58644dd32dd0
Ralph Olarte <olarteralphanthony@yahoo.com>
2026-02-12 13:44:53 -0500

    Set-Up: Controllers and Routes

---
+++ controllers/jobs.js
@@ -0,0 +1,29 @@
+const getAllJobs = async (req, res) => {
+  res.send('register user');
+}
+
+
+const getJob = async (req, res) => {
+  res.send('register user');
+}
+
+const createJob = async (req, res) => {
+  res.send('register user');
+}
+
+const updateJob = async (req, res) => {
+  res.send('register user');
+}
+
+
+const deleteJob = async (req, res) => {
+  res.send('register user');
+}
+
+module.exports = {
+  getAllJobs,
+  getJob,
+  createJob,
+  updateJob,
+  deleteJob
+}
