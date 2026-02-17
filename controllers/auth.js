8a39a73a1b4165297b635e50348bfca49e47da1d
Ralph Olarte <olarteralphanthony@yahoo.com>
2026-02-17 15:36:31 -0500

    Finish: Register Route, Schema Instance Method

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,13 +1,10 @@
 const User = require("../models/User");
 const { StatusCodes } = require("http-status-codes");
 const { BadRequestError } = require("../errors");
-const jwt = require("jsonwebtoken");
 
 const register = async (req, res) => {
-  const user = await User.create({ ...req.body });
-  const token = jwt.sign({ userId: user._id, name: user.name }, "jwtSecret", {
-    expiresIn: "30d",
-  });
+  const user = await User.create({ ...req.body }); // takes whatever's the input and crates a new user document
+  const token = user.createJWT(); // a custom method that makes a JSON web token
   res.status(StatusCodes.CREATED).json({ user: { name: user.name }, token });
 };
 const login = async (req, res) => {
