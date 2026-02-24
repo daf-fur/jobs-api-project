864708600e28e9eb74449a8eaa8bd801a316abd7
Ralph Olarte <olarteralphanthony@yahoo.com>
2026-02-24 17:28:45 -0500

    ADD: Finish createJob, start geAllJobs

--- controllers/jobs.js
+++ controllers/jobs.js
@@ -3,7 +3,8 @@ const { StatusCodes } = require("http-status-codes");
 const { BadRequestError, NotFoundError } = require("../errors");
 
 const getAllJobs = async (req, res) => {
-  res.send("register user");
+  const jobs = await Job.find({createdBy: req.user.userId}).sort('createdAt')
+  res.status(StatusCodes.OK).json({jobs, count: jobs.length})
 };
 
 const getJob = async (req, res) => {
@@ -13,7 +14,7 @@ const getJob = async (req, res) => {
 const createJob = async (req, res) => {
   req.body.createdBy = req.user.userId
   const job = await Job.create(req.body)
-  res.send(req.body);
+  res.status(StatusCodes.CREATED).json({job})
 };
 
 const updateJob = async (req, res) => {
